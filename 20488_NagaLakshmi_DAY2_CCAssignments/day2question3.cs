9abf909a709a467df802f8a50fa7cba50394df6e
lakshmibojjagani <51117176+lakshmibojjagani@users.noreply.github.com>
2021-10-01 08:43:26 +0530

    Update day2question3.cs

--- 20488_NagaLakshmi_DAY2_CCAssignments/day2question3.cs
+++ 20488_NagaLakshmi_DAY2_CCAssignments/day2question3.cs
@@ -1,20 +1,61 @@
 using System;
 
-public class Program
+namespace assignment3
 {
-    public static void Main()
+    public class EmployeeSalary
     {
-        Double Basic_Salary, DA, HRA, Gross_Salary;
-        
-        Console.Write("Enter Basic Salary : ");
-        Basic_Salary = Convert.ToInt32(Console.ReadLine());
-        
-        DA = (Basic_Salary * 40) / 100;
-        HRA = (Basic_Salary * 20) / 100;
-        Gross_Salary = Basic_Salary + DA + HRA;
-        
-        Console.Write("\n\nDearness Allowance 40 % of Basic Salary : " + DA);
-        Console.Write("\nHouse Rent 20 % of Basic Salary : " + HRA);
-        Console.Write("\nGross Salary : " + Gross_Salary);
+        string name;
+        int empid;
+        int basic;
+        int hr;
+        int trvlall;
+
+        public EmployeeSalary(string name,int empid,int basic,int hr,int trvlall)
+        {
+            this.name = name;
+            this.empid = empid;
+            this.basic = basic;
+            this.hr = hr;
+            this.trvlall = trvlall;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public int getEmpid()
+        {
+            return empid;
+        }
+        public int getBasic()
+        {
+            return basic;
+        }
+        public int getHr()
+        {
+            return basic;
+        }
+        public int getTrvl()
+        {
+            return basic;
+        }
+        public double tax()
+        {
+            if (basic * 12 < 50000 && basic * 12 > 0)
+                return basic * 0;
+            else if (basic * 12 > 50000 && basic * 12 < 70000)
+                return basic * .15;
+            else
+                return basic * .2;
+        }
+        public int gross()
+        {
+            return basic + hr + trvlall;
+        }
+        static void Main(string[] args)
+        {
+            EmployeeSalary emp = new EmployeeSalary("Chirag", 20792, 6000, 1000, 500);
+
+            Console.WriteLine("Hello"+emp.getName()+".\n Emp ID:"+emp.getEmpid()+".\n Basic salary:"+ emp.getBasic()+".\n HR Allowance:"+emp.getHr()+ ".\n Travel Allowance:" + emp.getTrvl()++".\n Tax:" + emp.tax()+".\n Gross"+emp.gross());
+        }
     }
 }
